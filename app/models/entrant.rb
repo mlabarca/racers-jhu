154e26180639be2522c3c6d8e5c35df2a3736eb2
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-02-06 23:41:53 -0300

    Add entrant delegates and accessors.
    Todo: Investigate if delegates to custom objects work
    with allow_nil option.

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -10,8 +10,8 @@ class Entrant
   field :group,   type: Placing,  as: :group
 
   embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc], after_add: :update_total
-  embeds_one  :race, class_name: 'RaceRef'  
-  embeds_one  :racer, as: :parent, class_name: 'RacerInfo'
+  embeds_one  :race, class_name: 'RaceRef', autobuild: true  
+  embeds_one  :racer, as: :parent, class_name: 'RacerInfo', autobuild: true
 
   def update_total(result)
     self.secs = results.map(&:secs).sum
@@ -19,5 +19,28 @@ class Entrant
 
   def the_race
     race.race
+  end
+
+  delegate :first_name, :first_name=, to: :racer
+  delegate :last_name, :last_name=, to: :racer
+  delegate :gender, :gender=, to: :racer, prefix: "racer"
+  delegate :birth_year, :birth_year=, to: :racer
+  delegate :city, :city=, to: :racer
+  delegate :state, :state=, to: :racer
+
+  delegate :name, :name=, to: :race, prefix: "race"
+  delegate :date, :date=, to: :race, prefix: "race"
+
+  def overall_place
+    overall.place if overall
+  end
+  def gender_place
+    gender.place if gender
+  end
+  def group_name
+    group.name if group
+  end
+  def group_place
+    group.place if group
   end
 end
