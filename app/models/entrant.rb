7dc91a29e9e788253b5db5924af7c7f1e483e137
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-02-06 23:03:37 -0300

    Associate racer and entrants

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -11,6 +11,7 @@ class Entrant
 
   embeds_many :results, class_name: 'LegResult', order: [:"event.o".asc], after_add: :update_total
   embeds_one  :race, class_name: 'RaceRef'  
+  embeds_one  :racer, as: :parent, class_name: 'RacerInfo'
 
   def update_total(result)
     self.secs = results.map(&:secs).sum
--- app/models/racer.rb
+++ app/models/racer.rb
@@ -1,7 +1,8 @@
 class Racer
   include Mongoid::Document
   embeds_one :info, class_name: 'RacerInfo', as: :parent, autobuild: true
-
+  has_many :races, class_name: 'Entrant', foreign_key: 'racer.racer_id', 
+    dependent: :nullify, order: :"race.date".desc
   before_create :initialize_id
   
   def initialize_id
