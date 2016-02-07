12e89838801ff2a4ed4706cac7729962f643bcdb
Mauricio Labarca <mauriciolabarca4tw@gmail.com>
2016-02-07 10:56:37 -0300

    Add entrant delegates for results

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -14,7 +14,7 @@ class Entrant
   embeds_one  :racer, as: :parent, class_name: 'RacerInfo', autobuild: true
 
   def update_total(result)
-    self.secs = results.map(&:secs).sum
+    self.secs = results.map(&:secs).compact.sum
   end
 
   def the_race
@@ -42,5 +42,41 @@ class Entrant
   end
   def group_place
     group.place if group
+  end
+
+  RESULTS = {
+    "swim"=>SwimResult,
+    "t1"=>LegResult,
+    "bike"=>BikeResult,
+    "t2"=>LegResult,
+    "run"=>RunResult
+  }
+
+  RESULTS.keys.each do |name|
+    #create_or_find result
+    define_method("#{name}") do
+      result = results.select {|result| name==result.event.name if result.event}.first
+      if !result
+        result = RESULTS["#{name}"].new(:event=>{:name=>name})
+        results << result
+      end
+      result
+    end
+    
+    #assign event details to result
+    define_method("#{name}=") do |event|
+      event = self.send("#{name}").build_event(event.attributes)
+    end
+
+    #expose setter/getter for each property of each result
+    RESULTS["#{name}"].attribute_names.reject {|r| /^_/ === r}.each do |prop|
+      define_method("#{name}_#{prop}") do
+        event = self.send(name).send(prop)
+      end
+      define_method("#{name}_#{prop}=") do |value|
+        event = self.send(name).send("#{prop}=",value)
+        update_total nil if /secs/===prop
+      end
+    end
   end
 end
